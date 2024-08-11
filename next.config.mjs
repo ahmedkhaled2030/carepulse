deb7b51dd9cfdb942d50d1ab72aeee4403503a5e
Ahmed Khaled <xhunter19192020@gmail.com>
2024-08-11 12:01:25 +0300

    projectName

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ export default withSentryConfig(nextConfig, {
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
   org: "carepusle",
-  project: "care-pulse",
+  project: "carepusle",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
