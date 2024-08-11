4c1019a04b1576e59c34e66b1fba75a9d0959e63
Ahmed Khaled <xhunter19192020@gmail.com>
2024-08-11 12:19:38 +0300

    change org name

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
 
-  org: "carePulse",
+  org: "carepulse",
   project: "carePulse",
 
   // Only print logs for uploading source maps in CI
